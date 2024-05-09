c3d965437c009b85a0b5361d0e96263973c22f6d
Tomy Irawan <untukmilikku@gmail.com>
2024-05-09 17:51:36 +0700

    Script update for brew packages

---
+++ Script File/README.md
@@ -0,0 +1,5 @@
+## Run this in RTR
+
+```bash
+runscript -CloudFile="update-brew"
+```
---
+++ Script File/script.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+#homebrew env
+eval $(/opt/homebrew/bin/brew shellenv)
+
+#Upddate brew package
+su "$(dscl . -list /Users | tail -n 1)" -c "brew update"
+
+#other case
+# user >> echo "$(dscl . -list /Users | tail -n 4 | head -n 1)"
+echo "Done update packages"
+
+#Upgrade specific package xz utils (CVE-2024-3094)
+su "$(dscl . -list /Users | tail -n 1)" -c "brew upgrade xz"
+echo "Done upgrade XZ utils"
